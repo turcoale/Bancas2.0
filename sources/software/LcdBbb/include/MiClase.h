5e1758ef769f16021be634c629762fc5f4553420
Amid Ale <amidale@gmail.com>
2014-07-22 10:19:29 -0300

    nueva imagen

---
+++ sources/software/LcdBbb/include/MiClase.h
@@ -0,0 +1,14 @@
+#ifndef MICLASE_H
+#define MICLASE_H
+
+
+class MiClase
+{
+    public:
+        MiClase();
+        virtual ~MiClase();
+    protected:
+    private:
+};
+
+#endif // MICLASE_H
---
+++ sources/software/LcdBbb/src/MiClase.cpp
@@ -0,0 +1,18 @@
+#include "MiClase.h"
+#include <iostream>
+
+
+using namespace std;
+
+MiClase::MiClase()
+{
+    private:  //Datos privados de mi clase
+        int a,b;
+    public:   //funciones miembros de mi clase
+
+}
+
+MiClase::~MiClase()
+{
+    //dtor
+}
